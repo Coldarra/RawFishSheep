2a4f4352392d010265dcd3e4fd4c3da07af040d6
c562147487 <562147487@qq.com>
2019-04-13 17:23:20 +0800

    delivery

--- back-end/RawFishSheep/app_delivery/models.py
+++ back-end/RawFishSheep/app_delivery/models.py
@@ -1,3 +1,3 @@
 from django.db import models
-
+from app_order.models import *
 # Create your models here.
--- back-end/RawFishSheep/app_delivery/views.py
+++ back-end/RawFishSheep/app_delivery/views.py
@@ -1,3 +1,20 @@
+from .models import *
+from decorator import *
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import sys
+sys.path.append('../')
+
+
+@get
+def test(request):
+    return HttpResponse('OK')
+
+@get
+# @corier
+def undistribution(request):
+    interface_id = "6010"
+
+    try:
+        Delivery.objects.filter()
