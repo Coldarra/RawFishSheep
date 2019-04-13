a02a3023b3279ec37dea811092795422f8a65b49
c562147487 <562147487@qq.com>
2019-04-13 17:32:58 +0800

    delivery

--- back-end/RawFishSheep/app_delivery/url.py
+++ back-end/RawFishSheep/app_delivery/url.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.test),
     path("undistribution", views.undistribution),
-    
+    path("distribution", views.distribution),
 ]
--- back-end/RawFishSheep/app_delivery/views.py
+++ back-end/RawFishSheep/app_delivery/views.py
@@ -12,9 +12,21 @@ def test(request):
     return HttpResponse('OK')
 
 @get
-# @corier
+@corier
 def undistribution(request):
     interface_id = "6010"
 
     try:
-        Delivery.objects.filter()
+        resp = {
+            "order": Order.objects.filter(status="1", isdelete="0")
+        }
+        return pack(interface_id, "0", "成功", resp)
+    except:
+        return pack(interface_id, "60102", "查询无果")
+
+@post
+@corier
+def distribution(request):
+    interface_id = "6011"
+    user_id = request.p
+    try:
