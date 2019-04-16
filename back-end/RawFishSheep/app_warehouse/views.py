b615ea737e2a57aa40ee8cea7449b852ad7c80a0
JamesSzg <jamesszg@hotmail.com>
2019-04-16 22:12:13 +0800

    tmp

--- back-end/RawFishSheep/app_warehouse/views.py
+++ back-end/RawFishSheep/app_warehouse/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from decorator import *
+from django.http import HttpResponse
 # Create your views here.
+
+@get
+def test(request):
+    return HttpResponse('OK')
+'''
+@admin
+@get
+def get_info(request):
+    interface_id = "3000"
+'''
