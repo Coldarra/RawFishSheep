bc002911b162c3669df68f9103f86d7c366bc4c4
Nile <coldarra@foxmail.com>
2019-05-05 02:40:02 +0800

    checklogin

--- back-end/RawFishSheep/app_user/views.py
+++ back-end/RawFishSheep/app_user/views.py
@@ -118,6 +118,7 @@ def log_in(request):
 
 @post
 def checklogin(request):
+    
     interface_id = "checklogin"
     if not request.session.get("isLogin", False):
         return pack(interface_id, data={"isLogin": False})
