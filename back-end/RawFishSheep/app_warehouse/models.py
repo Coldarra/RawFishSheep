95085dc0fee729ed54a1d3fc0902b17d0c20202c
Nile <coldarra@foxmail.com>
2019-05-08 20:23:15 +0800

    更新接口文档cargoin与cargoout

--- back-end/RawFishSheep/app_warehouse/models.py
+++ back-end/RawFishSheep/app_warehouse/models.py
@@ -8,19 +8,19 @@ from app_goods.models import *
 
 class Warehouse(models.Model):
     address = models.CharField(max_length=300, verbose_name='仓库地址')
-    
+
     def __str__(self):
         text = "__Warehouse__\n"
         for key, value in self.toDict().items():
             text += "{}: {}\n".format(key, value)
         return text
-    
+
     def toDict(self):
         return {
             "id": self.id,
             "address": self.address,
         }
-    
+
     def toDelete(self):
         self.delete()
         return
@@ -33,15 +33,21 @@ class Warehouse(models.Model):
 
 class Cargoin(models.Model):
     goods = models.ForeignKey(Goods, null=True, blank=True,
-                              on_delete=models.SET_NULL, related_name='cargoin_by_goods')
+                              on_delete=models.DO_NOTHING, related_name='cargoin_by_goods')
     warehouse = models.ForeignKey(Warehouse, null=True, blank=True,
-                                  on_delete=models.SET_NULL, related_name='cargoin_by_warehouse')
+                                  on_delete=models.DO_NOTHING, related_name='cargoin_by_warehouse')
     amount = models.IntegerField(blank=True, null=True, verbose_name='进货量')
-    cost = models.IntegerField(blank=True, null=True, verbose_name='进货成本')
+    cost = models.IntegerField(
+        blank=True, null=True, verbose_name='进货成本')  # 单位: 分
     entrytime = models.DateTimeField(
         blank=True, null=True, verbose_name='入库时间')
+    shelflife = models.IntegerField(
+        blank=True, null=True, default=72, verbose_name='保质期')  # 单位: 小时
     staletime = models.DateTimeField(
-        blank=True, null=True, verbose_name='保存时间')
+        blank=True, null=True, verbose_name='过期时间')
+    reason = models.CharField(
+        default='default', max_length=20, verbose_name='入库原因')
+    # 默认: 'default'  商品退货: 'refund'
     isdelete = models.CharField(default='0', max_length=1, verbose_name='是否删除')
 
     def toDelete(self):
@@ -62,7 +68,9 @@ class Cargoin(models.Model):
             "warehouse": self.warehouse,
             "amount": self.amount,
             "entrytime": self.entrytime.astimezone(tz).strftime("%Y/%m/%d %H:%M:%S"),
+            "shelflife": self.shelflife,
             "staletime": self.staletime.astimezone(tz).strftime("%Y/%m/%d %H:%M:%S"),
+            "reason": self.reason,
             "isdelete": self.isdelete,
             "cost": self.cost,
         }
@@ -75,11 +83,14 @@ class Cargoin(models.Model):
 
 class Cargoout(models.Model):
     goods = models.ForeignKey(Goods, null=True, blank=True,
-                              on_delete=models.SET_NULL, related_name='cargoout_by_goods')
+                              on_delete=models.DO_NOTHING, related_name='cargoout_by_goods')
     warehouse = models.ForeignKey(Warehouse, null=True, blank=True,
-                                  on_delete=models.SET_NULL, related_name='cargoout_by_warehouse')
+                                  on_delete=models.DO_NOTHING, related_name='cargoout_by_warehouse')
     order = models.ForeignKey(Order, null=True, blank=True,
-                              on_delete=models.SET_NULL, related_name='cargoout_by_order')
+                              on_delete=models.DO_NOTHING, related_name='cargoout_by_order')
+    reason = models.CharField(
+        default='default', max_length=20, verbose_name='出库原因')
+    # 默认: 'default'  商品返厂: 'refund'  商品下架: offshelves
     amount = models.IntegerField(blank=True, null=True, verbose_name='出货量')
     starttime = models.DateTimeField(
         blank=True, null=True, verbose_name='出库时间')
@@ -104,6 +115,7 @@ class Cargoout(models.Model):
             "order": self.order,
             "amount": self.amount,
             "starttime": self.starttime.astimezone(tz).strftime("%Y/%m/%d %H:%M:%S"),
+            "reason": self.reason,
             "isdelete": self.isdelete,
         }
 
