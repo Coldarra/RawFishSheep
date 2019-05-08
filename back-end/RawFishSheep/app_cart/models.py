518e1f5b20b8c11482d2fbb3a0e41aff5e103e35
Nile <coldarra@foxmail.com>
2019-05-08 21:54:28 +0800

    cart表新增createtime字段，调整model位置

--- back-end/RawFishSheep/app_cart/models.py
+++ back-end/RawFishSheep/app_cart/models.py
@@ -3,3 +3,37 @@ from app_order.models import *
 from app_user.models import *
 from app_goods.models import *
 # Create your models here.
+import pytz
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True,
+                             on_delete=models.DO_NOTHING, related_name='cart_by_user')
+    goods = models.ForeignKey(Goods, null=True, blank=True,
+                              on_delete=models.DO_NOTHING, related_name='cart_by_goods')
+    amount = models.IntegerField(default=0, verbose_name='数量')
+    selection = models.CharField(
+        default='1', max_length=1, verbose_name='是否勾选')
+    createtime = models.DateTimeField(auto_now=True,
+                                      blank=True, null=True, verbose_name='创建时间')
+
+    def __str__(self):
+        text = "__Cart__\n"
+        for key, value in self.toDict().items():
+            text += "{}: {}\n".format(key, value)
+        return text
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "goodsid": self.goods_id,
+            "goods": self.goods.name,
+            "amount": self.amount,
+            "selection": self.selection,
+        }
+
+    class Meta:
+        db_table = 'order_cart'
+        verbose_name = 'RawFishSheep'
+        app_label = 'app_order'
--- back-end/RawFishSheep/app_delivery/models.py
+++ back-end/RawFishSheep/app_delivery/models.py
@@ -2,3 +2,44 @@ from django.db import models
 from app_order.models import *
 from app_user.models import *
 # Create your models here.
+
+
+class Delivery(models.Model):
+    order = models.ForeignKey(Order, null=True, blank=True,
+                              on_delete=models.DO_NOTHING, related_name='delivery_by_order')
+    user = models.ForeignKey(User, null=True, blank=True,
+                             on_delete=models.DO_NOTHING, related_name='delivery_by_user')
+    createtime = models.DateTimeField(
+        blank=True, null=True, verbose_name='配送单创建时间')
+    receivetime = models.DateTimeField(
+        blank=True, null=True, verbose_name='接货时间')
+    finishtime = models.DateTimeField(
+        blank=True, null=True, verbose_name='送达时间')
+    isdelete = models.CharField(default='0', max_length=1, verbose_name='是否删除')
+
+    def toDelete(self):
+        self.isdelete = '1'
+        self.save()
+        return True
+
+    def __str__(self):
+        text = "__Delivery__\n"
+        for key, value in self.toDict().items():
+            text += "{}: {}\n".format(key, value)
+        return text
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "order": self.order_id,
+            "user": self.user.username,
+            "createtime": self.createtime.astimezone(tz).strftime("%Y/%m/%d %H:%M:%S"),
+            "receivetime": self.receivetime.astimezone(tz).strftime("%Y/%m/%d %H:%M:%S") if self.receivetime else None,
+            "finishtime": self.finishtime.astimezone(tz).strftime("%Y/%m/%d %H:%M:%S") if self.finishtime else None,
+            "isdelete": self.isdelete,
+        }
+
+    class Meta:
+        db_table = 'order_delivery'
+        verbose_name = 'RawFishSheep'
+        app_label = 'app_order'
