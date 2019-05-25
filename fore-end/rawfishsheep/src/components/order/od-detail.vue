9006af855839a05d7dc9afabe8415a99e793b40e
Nile <coldarra@foxmail.com>
2019-05-25 21:20:44 +0800

    订单详情页面

---
+++ fore-end/rawfishsheep/src/components/order/od-detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{this.$route.params.orderid}}</div>
+</template>
+
+<script>
+export default {
+  name: "od-detail",
+  orderid: "",
+  orderinfo: {},
+  data() {
+    return {};
+  },
+  mounted() {
+    this.orderid = this.$route.params.orderid;
+    this.$ajax.get("/api/order/info", { orderid: this.order }).then(res => {
+      if (res.data.ret == "0") {
+        console.log(res.data.data);
+        this.orderinfo = res.data.data.order;
+      }
+    });
+  }
+};
+</script>
+
